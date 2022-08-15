f30b840ecfc2609a00764893a18824969498cad6
superhelo <xiaohaluo@qq.com>
2022-08-15 22:53:48 +0800

    Remove dependency on CrT

--- src/main/java/superhelo/ic2no/Ic2No.java
+++ src/main/java/superhelo/ic2no/Ic2No.java
@@ -12,7 +12,7 @@ public class Ic2No {
 
     public static final String MOD_ID = "ic2-no";
     public static final String MOD_NAME = "IC2-No";
-    public static final String VERSION = "1.0";
-    public static final String DEPENDENCIES = "required-after:crafttweaker;required-after:mixinbooter;required-after:requious";
+    public static final String VERSION = "1.1";
+    public static final String DEPENDENCIES = "required-after:mixinbooter;required-after:requious";
 
 }
